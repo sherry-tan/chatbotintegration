d823c786aafabc7d8b79b303597cf66134d25b0f
Sherry Tan <sherry.th@gmail.com>
2019-01-22 21:02:11 +0800

    modify app.js

--- app.js
+++ app.js
@@ -8,7 +8,8 @@ const functions = require('firebase-functions');
 const {WebhookClient,Card, Suggestion} = require('dialogflow-fulfillment');
 const {dialogflow} = require('actions-on-google');
 process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
- 
+const app = dialogflow();
+
 exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
   const agent = new WebhookClient({ request, response });
   console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
@@ -21,22 +22,28 @@ exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, resp
   function fallback(agent) {
     agent.add(`I didn't understand`);
     agent.add(`I'm sorry, can you try again?`);
-}
+  }
+
+  function testImage(agent) {
+    agent.add(`WOrking...`);
+   
+  }
 
   // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
   intentMap.set('Default Welcome Intent', welcome);
   intentMap.set('Default Fallback Intent', fallback);
-  // intentMap.set('your intent name here', yourFunctionHandler);
-  // intentMap.set('your intent name here', googleAssistantHandler);
+  intentMap.set('getImageDetailIntent', testImage);
+  
   agent.handleRequest(intentMap);
 });
 
-const app = express();
+const exapp = express().use(bodyParser.json);
+exapp.post('/fulfillment',app.dialogflowFirebaseFulfillment());
 
-//var listener = app.listen(process.env.PORT,process.env.IP,function(){
-var listener = app.listen(4000,process.env.IP,function(){
+var listener = app.listen(process.env.PORT,process.env.IP,function(){
+// var listener = app.listen(4000,process.env.IP,function(){
   //var listener = app.listen(process.env.PORT,process.env.IP,function(){
-  console.log("server has started");
-   console.log('Listening on port ' + listener.address().port);
+    console.log("server has started");
+    console.log('Listening on port ' + listener.address().port);
 });
